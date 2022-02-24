8785624923b4bf106d349777b8efa568958c4341
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-02-24 09:30:45 -0300

    Adding the Auth0Provider component.

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-5tf99p7c.us.auth0.com"
+    clientId="GBPB42qhMWCtvrwGmYxvm5cbHXU68nzG"
+    redirectUri={window.location.origin + '/profile'}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
   document.getElementById('root')
 );
 
