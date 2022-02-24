f4fed5ab057d1d2487200e944aab47999b5a1784
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-02-24 09:32:09 -0300

    Adding the menu and route.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,136 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Link as RouterLink, Route, Routes } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Profile from './Profile';
+import SignIn from './SignIn';
 import './App.css';
 
 function App() {
+  const { isAuthenticated, logout } = useAuth0();
+
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="xl" sx={{ p: '0px !important' }}>
+      <BrowserRouter>
+        <AppBar position="static">
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+              >
+                React Auth0
+              </Typography>
+              <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{
+                    display: { xs: 'block', md: 'none' },
+                  }}
+                >
+                  <MenuItem onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">
+                      <Button component={RouterLink} to="/signIn">Sign In</Button>
+                    </Typography>
+                  </MenuItem>
+                  {isAuthenticated && 
+                  <MenuItem onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">
+                      <Button component={RouterLink} to="/profile">Profile</Button>
+                    </Typography>
+                  </MenuItem>
+                  }
+                </Menu>
+              </Box>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+              >
+                React Auth0
+              </Typography>
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                <Button
+                  component={RouterLink}
+                  to="/signIn"
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  Sign In
+                </Button>
+                {isAuthenticated && 
+                <Button
+                  component={RouterLink}
+                  to="/profile"
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  Profile
+                </Button>
+                }
+                {isAuthenticated && 
+                <Button
+                  onClick={() => {
+                    logout({ returnTo: window.location.origin });
+                  }}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  Sign Out
+                </Button>
+                }
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signIn" element={<SignIn />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
   );
 }
 
