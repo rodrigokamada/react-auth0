625b5d34c09911b8a694e178945ad5ed69f3abb4
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-02-24 09:52:35 -0300

    Fixing the test.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders react auth0', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getAllByText(/react auth0/i);
+  expect(element).toHaveLength(2);
 });
