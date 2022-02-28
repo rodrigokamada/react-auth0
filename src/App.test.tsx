97499ca96fd2f19c0263517a2e30455ff607a6e2
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-02-27 22:14:08 -0300

    Adding the timeout.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,9 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders react auth0', () => {
-  render(<App />);
-  //const element = screen.getAllByText(/react auth0/i);
-  //expect(element).toHaveLength(2);
+  setTimeout(() => {
+    render(<App />);
+    const element = screen.getAllByText(/react auth0/i);
+    expect(element).toHaveLength(2);
+  }, 1000);
 });
