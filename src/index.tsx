7abcec6ff2721a09af07c3776c5245f2ff6b2f8d
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-02-27 22:10:34 -0300

    Improvements.

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,23 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
+import Profile from './Profile';
+import SignIn from './SignIn';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Auth0Provider
-    domain="dev-5tf99p7c.us.auth0.com"
-    clientId="GBPB42qhMWCtvrwGmYxvm5cbHXU68nzG"
-    redirectUri={window.location.origin + '/react-auth0/profile'}
-  >
-    <App />
-  </Auth0Provider>,
+  <React.StrictMode>
+    <Auth0Provider
+      domain="dev-5tf99p7c.us.auth0.com"
+      clientId="GBPB42qhMWCtvrwGmYxvm5cbHXU68nzG"
+      redirectUri={window.location.origin + '/react-auth0/profile'}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
