ed40e9380ca7d68e8f671a2d5293915d61aec804
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-02-27 22:31:03 -0300

    Decreasing the timeout setting.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,5 +6,5 @@ test('renders react auth0', () => {
     render(<App />);
     const element = screen.getAllByText(/react auth0/i);
     expect(element).toHaveLength(2);
-  }, 2000);
+  }, 1000);
 });
